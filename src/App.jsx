77919a9821f57955686effea124026490d7601f4
karan <karanvaswani2002@gmail.com>
2023-02-23 22:03:58 +0530

    base structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import logo from "./images/logo.png";
 
 const App = () => {
     return (
       <div>
         <h1>Hello. This is a parcel react app.</h1>
+        <img src={logo} />
       </div>
     );
 };
